d63b6da009d305f59bfdfca55b6a4db8d08751a7
mattosoguilherme <guilhermemktfran@gmail.com>
2022-02-18 14:20:06 -0300

    ..

--- src/doctor/doctor.controller.ts
+++ src/doctor/doctor.controller.ts
@@ -2,32 +2,39 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { DoctorService } from './doctor.service';
 import { CreateDoctorDto } from './dto/create-doctor.dto';
 import { UpdateDoctorDto } from './dto/update-doctor.dto';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('doctor')
 @Controller('doctor')
 export class DoctorController {
   constructor(private readonly doctorService: DoctorService) {}
 
   @Post()
+  @ApiOperation({summary:"Cadastro de médico"})
   create(@Body() createDoctorDto: CreateDoctorDto) {
     return this.doctorService.create(createDoctorDto);
   }
 
   @Get()
+  @ApiOperation({summary:"Lista todos os médicos cadastrados"})
   findAll() {
     return this.doctorService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({summary:"Lista um médico pelo ID "})
   findOne(@Param('id') id: string) {
     return this.doctorService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({summary:"Atualiza o cadastro do médico"})
   update(@Param('id') id: string, @Body() updateDoctorDto: UpdateDoctorDto) {
     return this.doctorService.update(+id, updateDoctorDto);
   }
 
   @Delete(':id')
+  @ApiOperation({summary:"Apaga cadastro do médico"})
   remove(@Param('id') id: string) {
     return this.doctorService.remove(+id);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('CRM')
+    .setDescription('Sistema de gerenciamento de cadastro de médicos. Autor Guilherme Mattoso')
+    .setVersion('1.0')
+    .addTag('CRM')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();

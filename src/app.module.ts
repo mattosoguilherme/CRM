773d9f03b6eed393e27931c729f687cbfe20a0a3
mattosoguilherme <guilhermemktfran@gmail.com>
2022-02-18 14:19:57 -0300

    configurando ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DoctorModule } from './doctor/doctor.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DoctorModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

dcbd9f7d8d78cf2cdcbd6b96c81ef86e8e7fdfca
mattosoguilherme <guilhermemktfran@gmail.com>
2022-02-22 12:09:02 -0300

    ..

--- src/speciality/specialty.module.ts
+++ src/speciality/specialty.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SpecialtyService } from './specialty.service';
 import { SpecialtyController } from './specialty.controller';
 import { PrismaService } from 'src/prisma.service';
+import { Validation } from 'src/validations';
 
 @Module({
   controllers: [SpecialtyController],
-  providers: [SpecialtyService, PrismaService]
+  providers: [SpecialtyService, PrismaService, Validation]
 })
 export class SpecialtyModule {}

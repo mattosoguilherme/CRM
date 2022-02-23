60d13ed620a62f694e287fcc3f0da11a70e86690
mattosoguilherme <guilhermemktfran@gmail.com>
2022-02-23 18:57:07 -0300

    .

--- prisma/migrations/20220219181620_/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `doctor` MODIFY `crm` VARCHAR(7) NOT NULL,
-    MODIFY `landline` VARCHAR(10) NOT NULL,
-    MODIFY `cell_phone` VARCHAR(11) NOT NULL,
-    MODIFY `cep` VARCHAR(8) NOT NULL;
